c900ac492b1719a0b316a8ba070bed1ea9a82c79
blindgaenger <blindgaenger@gmail.com>
2009-07-19 23:49:34 +0200

    colorful implementation of list command

--- lib/commands/list.rb
+++ lib/commands/list.rb
@@ -5,7 +5,26 @@ command :list do |c|
   c.default_value 20
   c.flag :count
 
-  c.action do |global_options,options,args|
+  c.action do |globals, options, args|
+    def bold(text)
+      "<%= color('#{text}', BOLD) %>"
+    end
+
+    def color(text, color)
+      "<%= color('#{text}', :#{color.to_s}) %>"
+    end
+  
+    def indent(text, length=4)
+      ' '*length + wrap(text, length, 80)
+    end
+  
+    statuses = @twitter.friends_timeline({:count => options[:count]})
+    statuses.each {|status|
+      say bold(status.user.screen_name) + ' ' + color(status.created_at, :yellow)
+      puts
+      say indent(status.text)
+      puts
+    }
   end
 end
 
