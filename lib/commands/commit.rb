1b38ea8206953a283d3131104a197b05ed89d43f
blindgaenger <blindgaenger@gmail.com>
2009-07-21 00:01:49 +0200

    renamed post to commit

---
+++ lib/commands/commit.rb
@@ -0,0 +1,15 @@
+desc "Commit your current status"
+command :commit do |c|
+
+  c.desc "The text of your status update. Statuses over 140 characters will be forceably truncated."
+  c.flag :message, :m
+
+  c.action do |global, options, args|
+    unless options.message
+      say "Aborting commit due to empty commit message."
+    else
+      @twitter.update(options.message)
+    end
+  end
+
+end
--- lib/commands/post.rb
+++
@@ -1,8 +0,0 @@
-desc "Updates your current status"
-arg_name "status"
-command :post do |c|
-  c.action do |global_options,options,args|
-    message = args.join(' ').strip
-    @twitter.update(message)
-  end
-end
