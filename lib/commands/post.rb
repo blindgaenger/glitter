043502371613e6ecfa07e5bc759312602e00edc2
blindgaenger <blindgaenger@gmail.com>
2009-07-20 19:18:10 +0200

    fixed post

--- lib/commands/post.rb
+++ lib/commands/post.rb
@@ -2,8 +2,7 @@ desc "Updates your current status"
 arg_name "status"
 command :post do |c|
   c.action do |global_options,options,args|
-
-    p args.join(' ').strip
-
+    message = args.join(' ').strip
+    @twitter.update(message)
   end
 end
